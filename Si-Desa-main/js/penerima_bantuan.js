64ca85fb29590fa6268e606c8b53f563e327f53d
odeandialamsyah <odeandialamsyah@gmail.com>
2025-02-09 23:34:18 +0700

    updates

--- Si-Desa-main/js/penerima_bantuan.js
+++
@@ -1,8 +0,0 @@
-function showModal(nama, foto, rt_rw, jumlahKeluarga) {
-    document.getElementById('modalTitle').innerText = "Bukti Penerima Bantuan: " + nama;
-    document.getElementById('modalImage').src = foto;
-    document.getElementById('modalDescription').innerText = 
-        "Nama: " + nama + "\n" +
-        "RT/RW: " + rt_rw + "\n" +
-        "Jumlah Keluarga: " + jumlahKeluarga + " orang";
-}
